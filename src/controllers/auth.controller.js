0381e4bf1cf9cf566bb0cceb358d67264b84150b
Yohan Moren <yohan.moren@gmail.com>
2022-12-19 13:59:53 +0100

    Added Addprestige route, separating files later

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,3 +64,18 @@ exports.leaderboard = (req, res) => {
       res.status(400).send(err);
     });
 };
+
+exports.addPrestige = (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: "user not found"
+        })
+      }
+      User.findById(user._id).then(userupdated => {
+        res.send(userupdated);
+      })
+    })
+    .catch(err => res.status(400).send(err))
+}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/auth.controller");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/leaderboard", authController.leaderboard);
+router.put("/:id", authController.addPrestige);
 
 module.exports = router;
